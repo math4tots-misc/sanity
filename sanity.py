1cc63e3dbcbaca1f4016ddef106cb467e88d1900
Kyumin Kim <math4tots@gmail.com>
2016-01-31 10:41:05 -0800

    xxx

--- sanity.py
+++ sanity.py
@@ -1,70 +1,7 @@
 """sanity.py
 
-Let's try a simpler language first.
-
-Pattern
-  VariablePattern
-    name - String
-  TemplatePattern
-    name - String
-    type_arguments - [Pattern]
-
-Type:
-  name - String
-  type_arguments - [Type]
-
-Typedef:
-  name - String
-  type_arguments - [Pattern]
-  type - Type
-
-Class:
-  name - String
-  type_arguments - [Pattern]
-  returns - Type
-  fields - [(String, Type)]
-
-Function:
-  name - String
-  arguments - [(String, Statement)]
-  body - [Statement]
-
-Statement
-  Block:
-    statements - [Statement]
-  Return:
-    value - Expression
-  While:
-    condition - Expression
-    body - Statement
-  If:
-    condition - Expression
-    body - Statement
-    other - Statement?
-  Expression
-    FunctionCall:
-      name - String
-      type_arguments - [Type]
-    Name:
-      name - String
-
-=======
-
-For runtime polymorphism, consider:
-
-# 'Union' should probably be implemented natively.
-class Union[Left, Right] {
-  Bool which # false -> Left, true -> Right
-  Left left
-  Right right
-}
-
-typedef ...
-
-Or maybe, I could implement some sort of function pointer...
-
-I think there could be a safe way to do this.
-
-=======
+Simple translator.
 
 """
+
+
